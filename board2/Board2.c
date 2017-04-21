7a3456fc43bb17d5edf38af32c951f6dd8cf9713
Jonny <jonny.hong91@gmail.com>
2017-04-21 20:43:45 +0800

    board2 board3

---
+++ board2/Board2.c
@@ -0,0 +1,17 @@
+#include <STC89C5xRC.H>
+#include "Board2.h"
+#include "uart.h"
+#include "mp3.h"
+#include "BackRoom.h"
+
+void start();
+void init();
+
+/*
+ * 输出低电平触发
+ * 输入高电平触发
+ *
+ */
+void main()
+{
+}
